a1f6fae7b5c85b5fa62d325651e3348383772232
jhlee-codes <jhleezz00@naver.com>
2025-07-09 00:08:08 +0900

    [MN-1] chore: jira 이슈 close 자동화 및 husky 라이브러리 설정

---
+++ src/main/java/com/sprint/mission/sb03monewteam1/Sb03MonewTeam1Application.java
@@ -0,0 +1,13 @@
+package com.sprint.mission.sb03monewteam1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb03MonewTeam1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb03MonewTeam1Application.class, args);
+    }
+
+}
