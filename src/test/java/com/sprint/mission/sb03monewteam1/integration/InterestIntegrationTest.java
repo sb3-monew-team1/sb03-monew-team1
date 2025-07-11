022edc8bedc8730795d210881ef6a1e38678d5e3
Chaewon3Lee <coduszxcv@gmail.com>
2025-07-11 15:35:03 +0900

    [MN-20] test: 관심사 등록 - Integration red 테스트 작성

---
+++ src/test/java/com/sprint/mission/sb03monewteam1/integration/InterestIntegrationTest.java
@@ -0,0 +1,36 @@
+package com.sprint.mission.sb03monewteam1.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sprint.mission.sb03monewteam1.dto.request.InterestCreateRequestDto;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class InterestIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void 관심사를_등록하면_200OK와_DTO가_반환된다() throws Exception {
+        var request = new InterestCreateRequestDto("축구", List.of("스포츠", "해외축구"));
+
+        mockMvc.perform(post("/api/interests")
+                .content(objectMapper.writeValueAsString(request)))
+            .andExpect(status().isOk());
+    }
+}
