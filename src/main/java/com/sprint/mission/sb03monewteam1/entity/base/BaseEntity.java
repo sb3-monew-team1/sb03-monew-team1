86cbe1609d33856feb25cfd0da099e238f77544d
Chaewon3Lee <coduszxcv@gmail.com>
2025-07-12 00:15:41 +0900

    [MN-20] feat: BaseEntity 및 BaseUpdatableEntity 공통 엔티티 설계 추가

--- src/main/java/com/sprint/mission/sb03monewteam1/entity/base/BaseEntity.java
+++ src/main/java/com/sprint/mission/sb03monewteam1/entity/base/BaseEntity.java
@@ -1,5 +1,30 @@
 package com.sprint.mission.sb03monewteam1.entity.base;
 
-public class BaseEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(columnDefinition = "uuid", updatable = false, nullable = false)
+    private UUID id;
+
+    @CreatedDate
+    @Column(columnDefinition = "timestamp with time zone", updatable = false, nullable = false)
+    private Instant createdAt;
+
 
 }
--- src/main/java/com/sprint/mission/sb03monewteam1/entity/base/BaseUpdatableEntity.java
+++ src/main/java/com/sprint/mission/sb03monewteam1/entity/base/BaseUpdatableEntity.java
@@ -1,5 +1,19 @@
 package com.sprint.mission.sb03monewteam1.entity.base;
 
-public class BaseUpdatableEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.Instant;
+import lombok.Getter;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseUpdatableEntity extends BaseEntity {
+
+    @LastModifiedDate
+    @Column(columnDefinition = "timestamp with time zone")
+    private Instant updatedAt;
 }
