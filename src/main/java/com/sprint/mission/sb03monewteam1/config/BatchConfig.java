f0c1f35f5aeb85298814a39ba363b50b346b49d4
NINE <99791512+NINE-J@users.noreply.github.com>
2025-07-13 03:45:05 +0900

    [MN-45] feat: BatchJob 테스트 - GREEN

--- src/main/java/com/sprint/mission/sb03monewteam1/config/BatchConfig.java
+++ src/main/java/com/sprint/mission/sb03monewteam1/config/BatchConfig.java
@@ -1,10 +1,33 @@
 package com.sprint.mission.sb03monewteam1.config;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
-@EnableBatchProcessing
 public class BatchConfig {
-    // TODO: Batch Job 설정
+
+    @Bean
+    public Job dummyJob(JobRepository jobRepository, Step dummyStep) {
+        return new JobBuilder("dummyJob", jobRepository)
+            .start(dummyStep)
+            .build();
+    }
+
+    @Bean
+    public Step dummyStep(JobRepository jobRepository,
+        PlatformTransactionManager transactionManager) {
+        return new StepBuilder("dummyStep", jobRepository)
+            .tasklet((contribution, chunkContext) -> {
+                System.out.println("Dummy Step executed!");
+                return RepeatStatus.FINISHED;
+            }, transactionManager)
+            .build();
+    }
 }
--- src/test/java/com/sprint/mission/sb03monewteam1/batch/job/BatchJobTest.java
+++ src/test/java/com/sprint/mission/sb03monewteam1/batch/job/BatchJobTest.java
@@ -1,27 +1,34 @@
-package com.sprint.mission.sb03monewteam1.batch;
+package com.sprint.mission.sb03monewteam1.batch.job;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import com.sprint.mission.sb03monewteam1.config.TestConfig;
+import com.sprint.mission.sb03monewteam1.config.TestEnvSetup;
 
 @SpringBatchTest
 @SpringBootTest
+@Import(TestConfig.class)
 class BatchJobTest {
 
     @Autowired
-    private JobLauncher jobLauncher;
+    private JobLauncherTestUtils jobLauncherTestUtils;
 
-    @Autowired
-    private Job dummyJob;
+    @BeforeAll
+    static void setUp() {
+        TestEnvSetup.loadEnvVariables();
+    }
 
     @Test
-    void BatchJob_실행_테스트() throws Exception {
-        var jobExecution = jobLauncher.run(dummyJob, new org.springframework.batch.core.JobParameters());
+    void dummyJob_실행_테스트() throws Exception {
+        var jobExecution = jobLauncherTestUtils.launchJob();
         assertThat(jobExecution.getExitStatus().getExitCode()).isEqualTo("COMPLETED");
     }
 }
