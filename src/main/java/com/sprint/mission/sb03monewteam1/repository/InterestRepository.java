51bacf6cb34ddfef06b0ed546163e909ea5858cd
Chaewon3Lee <coduszxcv@gmail.com>
2025-07-12 13:58:18 +0900

    [MN-20] chore: 필요 없는 주석 제거

--- src/main/java/com/sprint/mission/sb03monewteam1/repository/InterestRepository.java
+++ src/main/java/com/sprint/mission/sb03monewteam1/repository/InterestRepository.java
@@ -1,7 +1,6 @@
 package com.sprint.mission.sb03monewteam1.repository;
 
 import com.sprint.mission.sb03monewteam1.entity.Interest;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InterestRepository extends JpaRepository<Interest, Long> {
