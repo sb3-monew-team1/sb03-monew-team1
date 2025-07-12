7128739b5121a6ba7bcd5215ad2203040e149ece
NINE <99791512+NINE-J@users.noreply.github.com>
2025-07-13 03:58:13 +0900

    [MN-45] feat: ArticleJob 테스트 - GREEN

--- src/test/java/com/sprint/mission/sb03monewteam1/batch/job/ArticleBatchJobTest.java
+++ src/test/java/com/sprint/mission/sb03monewteam1/batch/job/ArticleBatchJobTest.java
@@ -1,15 +1,16 @@
 package com.sprint.mission.sb03monewteam1.batch.job;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import com.sprint.mission.sb03monewteam1.config.TestConfig;
+import com.sprint.mission.sb03monewteam1.config.TestEnvSetup;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-
-import com.sprint.mission.sb03monewteam1.config.TestConfig;
 
 @SpringBatchTest
 @SpringBootTest
@@ -19,12 +20,14 @@ class ArticleBatchJobTest {
     @Autowired
     private JobLauncherTestUtils jobLauncherTestUtils;
 
-    @Test
-    void ArticleJob_실행_테스트() throws Exception {
-        // when
-        var jobExecution = jobLauncherTestUtils.launchJob();
+    @BeforeAll
+    static void setUp() {
+        TestEnvSetup.loadEnvVariables();
+    }
 
-        // then
+    @Test
+    void articleJob_실행_테스트() throws Exception {
+        var jobExecution = jobLauncherTestUtils.launchJob();
         assertThat(jobExecution.getExitStatus().getExitCode()).isEqualTo("COMPLETED");
     }
 }
