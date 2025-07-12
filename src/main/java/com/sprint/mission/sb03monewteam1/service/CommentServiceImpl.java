5944d173f8e3e7df3a70a3c6877ca1a750abfa50
jhlee-codes <jhleezz00@naver.com>
2025-07-12 13:40:46 +0900

    [MN-9] test: 댓글 등록 - Service Red 테스트 작성

---
+++ src/main/java/com/sprint/mission/sb03monewteam1/service/CommentServiceImpl.java
@@ -0,0 +1,14 @@
+package com.sprint.mission.sb03monewteam1.service;
+
+import com.sprint.mission.sb03monewteam1.dto.CommentDto;
+import com.sprint.mission.sb03monewteam1.dto.request.CommentRegisterRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentServiceImpl implements CommentService {
+
+    @Override
+    public CommentDto create(CommentRegisterRequest commentRegisterRequest) {
+        return null;
+    }
+}
---
+++ src/test/java/com/sprint/mission/sb03monewteam1/service/CommentServiceTest.java
@@ -0,0 +1,109 @@
+package com.sprint.mission.sb03monewteam1.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+import com.sprint.mission.sb03monewteam1.dto.CommentDto;
+import com.sprint.mission.sb03monewteam1.dto.request.CommentRegisterRequest;
+import com.sprint.mission.sb03monewteam1.entity.Article;
+import com.sprint.mission.sb03monewteam1.entity.Comment;
+import com.sprint.mission.sb03monewteam1.entity.User;
+import com.sprint.mission.sb03monewteam1.mapper.CommentMapper;
+import com.sprint.mission.sb03monewteam1.repository.CommentRepository;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+@DisplayName("CommentService 슬라이스 테스트")
+@ExtendWith(MockitoExtension.class)
+public class CommentServiceTest {
+
+    @Mock
+    private CommentRepository commentRepository;
+
+    @Mock
+    private CommentMapper commentMapper;
+
+    @InjectMocks
+    private CommentService commentService;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Nested
+    @DisplayName("댓글 생성 테스트")
+    class CommentCreateTest {
+
+        @Test
+        void 댓글을_생성하면_CommentDto를_반환해야한다() {
+
+            // given
+            String content = "댓글 생성 테스트";
+            String userNickname = "testUser";
+
+            User user = User.builder()
+                    .nickname(userNickname)
+                    .build();
+
+            Article article = Article.builder().build();
+
+            ReflectionTestUtils.setField(user, "id", UUID.randomUUID());
+            ReflectionTestUtils.setField(article, "id", UUID.randomUUID());
+
+            UUID userId = user.getId();
+            UUID articleId = article.getId();
+
+            CommentRegisterRequest commentRegisterRequest = CommentRegisterRequest.builder()
+                    .content(content)
+                    .articleId(articleId)
+                    .userId(userId)
+                    .build();
+
+            Comment savedComment = Comment.builder()
+                    .content(content)
+                    .likeCount(0L)
+                    .isDeleted(false)
+                    .article(article)
+                    .author(user)
+                    .build();
+
+            ReflectionTestUtils.setField(savedComment, "id", UUID.randomUUID());
+
+            CommentDto expectedCommentDto = CommentDto.builder()
+                    .id(savedComment.getId())
+                    .createdAt(savedComment.getCreatedAt())
+                    .articleId(articleId)
+                    .userId(userId)
+                    .content(savedComment.getContent())
+                    .userNickname(userNickname)
+                    .likeCount(0L)
+                    .likedByMe(false)
+                    .build();
+
+            given(commentRepository.save(any(Comment.class))).willReturn(savedComment);
+            given(commentMapper.toDto(any(Comment.class))).willReturn(expectedCommentDto);
+
+            // when
+            CommentDto result = commentService.create(commentRegisterRequest);
+
+            // then
+            assertThat(result).isNotNull();
+            assertThat(result.content()).isEqualTo(expectedCommentDto.content());
+            assertThat(result.articleId()).isEqualTo(expectedCommentDto.articleId());
+            assertThat(result.userId()).isEqualTo(expectedCommentDto.userId());
+            assertThat(result.likeCount()).isEqualTo(expectedCommentDto.likeCount());
+            assertThat(result.likedByMe()).isEqualTo(expectedCommentDto.likedByMe());
+            assertThat(result.createdAt()).isEqualTo(expectedCommentDto.createdAt());
+        }
+    }
+}
