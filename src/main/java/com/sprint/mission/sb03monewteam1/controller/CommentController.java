5fa73255adb897f131e12f1fc639c75904bb67b6
jhlee-codes <jhleezz00@naver.com>
2025-07-15 23:46:15 +0900

    [MN-12] feat: 댓글 논리 삭제 - Controller Green 기능 구현

--- src/main/java/com/sprint/mission/sb03monewteam1/controller/CommentController.java
+++ src/main/java/com/sprint/mission/sb03monewteam1/controller/CommentController.java
@@ -5,6 +5,7 @@ import com.sprint.mission.sb03monewteam1.dto.CommentDto;
 import com.sprint.mission.sb03monewteam1.dto.request.CommentRegisterRequest;
 import com.sprint.mission.sb03monewteam1.dto.request.CommentUpdateRequest;
 import com.sprint.mission.sb03monewteam1.dto.response.CursorPageResponse;
+import com.sprint.mission.sb03monewteam1.entity.Comment;
 import com.sprint.mission.sb03monewteam1.service.CommentService;
 import jakarta.validation.Valid;
 import java.time.Instant;
@@ -13,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -85,5 +87,19 @@ public class CommentController implements CommentApi {
         return ResponseEntity
             .status(HttpStatus.OK)
             .body(result);
+    }
+
+    @Override
+    @DeleteMapping(path = "/{commentId}")
+    public ResponseEntity<Void> delete(@PathVariable UUID commentId) {
+
+        log.info("댓글 삭제 요청: commentId = {}", commentId);
+
+        commentService.delete(commentId);
+
+        log.info("댓글 삭제 성공 - commentId: {}", commentId);
+        return ResponseEntity
+            .status(HttpStatus.NO_CONTENT)
+            .build();
     }
 }
