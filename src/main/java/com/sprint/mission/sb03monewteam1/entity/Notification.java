27cc67f7c44088f1d4380862893ef23140583e39
pureod <sjo06102@gmail.com>
2025-07-17 11:14:17 +0900

    feat: Notification Entity 구현

--- src/main/java/com/sprint/mission/sb03monewteam1/entity/Notification.java
+++ src/main/java/com/sprint/mission/sb03monewteam1/entity/Notification.java
@@ -1,5 +1,49 @@
 package com.sprint.mission.sb03monewteam1.entity;
 
-public class Notification {
+import com.sprint.mission.sb03monewteam1.entity.base.BaseUpdatableEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Entity
+@Table(name = "notifications")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notification extends BaseUpdatableEntity {
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "resource_type", nullable = false)
+    private String resourceType;
+
+    @Column(name = "resource_id")
+    private UUID resourceId;
+
+    @Builder.Default
+    @Column(name = "is_checked", nullable = false)
+    private boolean isChecked = false;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public void markAsChecked() {
+        this.isChecked = true;
+    }
 
 }
