5cfd71fe532e1afcc15961ad66fdebc83354950b
pureod <sjo06102@gmail.com>
2025-07-18 11:46:05 +0900

    [MN-30] test: 괸심 기사 등록 - Service Red 단계 작성

---
+++ src/test/java/com/sprint/mission/sb03monewteam1/fixture/NotificationFixture.java
@@ -0,0 +1,57 @@
+package com.sprint.mission.sb03monewteam1.fixture;
+
+import com.sprint.mission.sb03monewteam1.dto.ResourceType;
+import com.sprint.mission.sb03monewteam1.entity.Interest;
+import com.sprint.mission.sb03monewteam1.entity.Notification;
+import com.sprint.mission.sb03monewteam1.entity.User;
+import java.util.UUID;
+
+public class NotificationFixture {
+
+    public static Notification createNewArticleNotification() {
+        return Notification.builder()
+                .content("새로운 기사 등록 알림입니다.")
+                .resourceType(ResourceType.interest)
+                .resourceId(UUID.randomUUID())
+                .isChecked(false)
+                .user(UserFixture.createUser())
+                .build();
+    }
+
+    public static Notification createNewArticleNotification(User user) {
+        return Notification.builder()
+                .content("새로운 기사 등록 알림입니다.")
+                .resourceType(ResourceType.interest)
+                .resourceId(UUID.randomUUID())
+                .isChecked(false)
+                .user(user)
+                .build();
+    }
+
+    public static Notification createNewArticleNotification(String content) {
+        return Notification.builder()
+            .content(content)
+            .resourceType(ResourceType.interest)
+            .resourceId(UUID.randomUUID())
+            .isChecked(false)
+            .user(UserFixture.createUser())
+            .build();
+    }
+
+    public static Notification createNewArticleNotification(
+        User user,
+        Interest interest,
+        int articleCount
+    ) {
+        return Notification.builder()
+            .content("새로운 기사 등록 알림입니다.")
+            .resourceType(ResourceType.interest)
+            .resourceId(UUID.randomUUID())
+            .isChecked(false)
+            .user(user)
+            .build();
+    }
+
+
+
+}
