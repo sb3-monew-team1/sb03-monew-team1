bf8eca004986b60f361bf2393f96459e097afe59
jhlee-codes <jhleezz00@naver.com>
2025-07-18 14:04:17 +0900

    [MN-63] test: 좋아요 알림 등록 - IntegrationTest Red 테스트 작성

---
+++ src/test/java/com/sprint/mission/sb03monewteam1/integration/NotificationIntegrationTest.java
@@ -0,0 +1,102 @@
+package com.sprint.mission.sb03monewteam1.integration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+import com.sprint.mission.sb03monewteam1.config.LoadTestEnv;
+import com.sprint.mission.sb03monewteam1.dto.ResourceType;
+import com.sprint.mission.sb03monewteam1.entity.Article;
+import com.sprint.mission.sb03monewteam1.entity.Comment;
+import com.sprint.mission.sb03monewteam1.entity.Notification;
+import com.sprint.mission.sb03monewteam1.entity.User;
+import com.sprint.mission.sb03monewteam1.fixture.ArticleFixture;
+import com.sprint.mission.sb03monewteam1.fixture.CommentFixture;
+import com.sprint.mission.sb03monewteam1.fixture.UserFixture;
+import com.sprint.mission.sb03monewteam1.repository.ArticleRepository;
+import com.sprint.mission.sb03monewteam1.repository.CommentRepository;
+import com.sprint.mission.sb03monewteam1.repository.NotificationRepository;
+import com.sprint.mission.sb03monewteam1.repository.UserRepository;
+import com.sprint.mission.sb03monewteam1.service.CommentServiceImpl;
+import com.sprint.mission.sb03monewteam1.service.NotificationServiceImpl;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.transaction.annotation.Transactional;
+
+@LoadTestEnv
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@ActiveProfiles("test")
+@DisplayName("InterestIntegration 테스트")
+public class NotificationIntegrationTest {
+
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
+    @Autowired
+    private NotificationRepository notificationRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @Autowired
+    private NotificationServiceImpl notificationService;
+
+    @Autowired
+    private CommentServiceImpl commentService;
+
+    @Nested
+    @DisplayName("좋아요 알림 등록 테스트")
+    class NotifyCommentLikeTest {
+
+        @Test
+        void 좋아요를_등록하면_알림이_Repository에_저장된다() throws InterruptedException {
+
+            // given
+            UUID articleId = UUID.randomUUID();
+            UUID userId = UUID.randomUUID();
+            UUID commentId = UUID.randomUUID();
+
+            Article article = ArticleFixture.createArticle();
+            ReflectionTestUtils.setField(article, "id", articleId);
+            User user = UserFixture.createUser();
+            ReflectionTestUtils.setField(user, "id", userId);
+            Comment comment = CommentFixture.createComment(user, article);
+            ReflectionTestUtils.setField(comment, "id", commentId);
+
+            articleRepository.save(article);
+            userRepository.save(user);
+            commentRepository.save(comment);
+
+            // when
+            commentService.like(article.getId(), user.getId());
+
+            // then
+            await()
+                .atMost(1, TimeUnit.SECONDS)
+                .untilAsserted(() -> {
+                    Notification notification = notificationRepository.findAll().get(0);
+                    assertThat(notification.getUser()).isEqualTo(user);
+                    assertThat(notification.getResourceType()).isEqualTo(ResourceType.comment);
+                    assertThat(notification.getResourceId()).isEqualTo(comment.getId());
+                    assertThat(notification.getContent()).contains(user.getNickname());
+                });
+        }
+    }
+}
