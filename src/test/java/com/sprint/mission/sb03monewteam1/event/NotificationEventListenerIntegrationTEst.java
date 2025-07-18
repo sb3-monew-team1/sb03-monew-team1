5204b6ac863e90d4fb89ad079d4caec0ed140f08
pureod <sjo06102@gmail.com>
2025-07-18 14:44:20 +0900

    [MN-30] test: 관심 기사 등록 알림 - Integration 테스트 통과

---
+++ src/test/java/com/sprint/mission/sb03monewteam1/event/NotificationEventListenerIntegrationTEst.java
@@ -0,0 +1,108 @@
+package com.sprint.mission.sb03monewteam1.event;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.sprint.mission.sb03monewteam1.config.LoadTestEnv;
+import com.sprint.mission.sb03monewteam1.dto.ArticleDto;
+import com.sprint.mission.sb03monewteam1.dto.ResourceType;
+import com.sprint.mission.sb03monewteam1.entity.Interest;
+import com.sprint.mission.sb03monewteam1.entity.Notification;
+import com.sprint.mission.sb03monewteam1.entity.Subscription;
+import com.sprint.mission.sb03monewteam1.entity.User;
+import com.sprint.mission.sb03monewteam1.fixture.ArticleFixture;
+import com.sprint.mission.sb03monewteam1.fixture.InterestFixture;
+import com.sprint.mission.sb03monewteam1.fixture.SubscriptionFixture;
+import com.sprint.mission.sb03monewteam1.fixture.UserFixture;
+import com.sprint.mission.sb03monewteam1.repository.InterestRepository;
+import com.sprint.mission.sb03monewteam1.repository.NotificationRepository;
+import com.sprint.mission.sb03monewteam1.repository.SubscriptionRepository;
+import com.sprint.mission.sb03monewteam1.repository.UserRepository;
+import java.time.Duration;
+import java.util.List;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@LoadTestEnv
+@Transactional
+@ActiveProfiles("test")
+@DisplayName("NotificationIntegration 테스트")
+public class NotificationEventListenerIntegrationTEst {
+
+    @Autowired
+    private ApplicationEventPublisher eventPublisher;
+
+    @Autowired
+    private NotificationRepository notificationRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private InterestRepository interestRepository;
+
+    @Autowired
+    private SubscriptionRepository subscriptionRepository;
+
+
+    @Test
+    void 관심_기사_등록_이벤트가_발행되면_레포지토리에_알림이_저장되어야_한다() {
+        // Given
+        User user1 = UserFixture.createUser(
+            "user1@exapmle.com",
+            "user1",
+            "!Password123"
+        );
+        userRepository.save(user1);
+
+        User user2 = UserFixture.createUser(
+            "user2@exapmle.com",
+            "user2",
+            "!Password123"
+        );
+        userRepository.save(user2);
+
+        User user3 = UserFixture.createUser(
+            "user3@exapmle.com",
+            "user3",
+            "!Password123"
+        );
+        userRepository.save(user3);
+
+        Interest interest = InterestFixture.createInterest();
+        interestRepository.save(interest);
+
+        Subscription subscription1 = SubscriptionFixture.createSubscription(user1, interest);
+        subscriptionRepository.save(subscription1);
+
+        Subscription subscription2 = SubscriptionFixture.createSubscription(user2, interest);
+        subscriptionRepository.save(subscription2);
+
+        Subscription subscription3 = SubscriptionFixture.createSubscription(user3, interest);
+        subscriptionRepository.save(subscription3);
+
+        List<ArticleDto> articles = ArticleFixture.createArticleDtoList();
+
+        // When
+        eventPublisher.publishEvent(new NewArticleCollectEvent(interest, articles));
+
+        // Then
+        Awaitility.await()
+            .atMost(Duration.ofSeconds(5))
+            .untilAsserted(() -> {
+                List<Notification> notifications = notificationRepository.findAll();
+                assertThat(notifications.size()).isEqualTo(3);
+                assertThat(notifications.get(0).getResourceType()).isEqualTo(ResourceType.interest);
+                assertThat(notifications.get(0).getUser().getId()).isEqualTo(user1.getId());
+            });
+
+    }
+
+}
