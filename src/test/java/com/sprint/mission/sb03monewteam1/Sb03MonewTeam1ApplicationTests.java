aabbd577c6cbeb9125ef71ec8ea3336bae8f4002
Chaewon3Lee <coduszxcv@gmail.com>
2025-07-19 12:04:12 +0900

    [MN-27] chore: mongodb 테스트를 위한 Flapdoodle embedded MongoDB 추가

--- src/test/java/com/sprint/mission/sb03monewteam1/Sb03MonewTeam1ApplicationTests.java
+++ src/test/java/com/sprint/mission/sb03monewteam1/Sb03MonewTeam1ApplicationTests.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.sprint.mission.sb03monewteam1.config.LoadTestEnv;
+import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootTest
+@SpringBootTest(properties = "de.flapdoodle.mongodb.embedded.version=5.0.5")
+@EnableAsync
 @LoadTestEnv
 class Sb03MonewTeam1ApplicationTests {
 
