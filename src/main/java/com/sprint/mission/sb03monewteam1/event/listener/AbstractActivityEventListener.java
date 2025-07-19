0578cf07a1ccf72e861f8510d61b6b843d9f703b
Chaewon3Lee <coduszxcv@gmail.com>
2025-07-19 10:54:27 +0900

    [MN-27] feat: 사용자 활동 내역 - deleteUserActivity 누락 로직 추가

--- src/main/java/com/sprint/mission/sb03monewteam1/event/listener/AbstractActivityEventListener.java
+++ src/main/java/com/sprint/mission/sb03monewteam1/event/listener/AbstractActivityEventListener.java
@@ -26,4 +26,20 @@ public abstract class AbstractActivityEventListener<T, D, R extends MongoReposit
 
         log.debug("ActivityCreate 성공 userId: {}", userId);
     }
+
+    public void deleteUserActivity(UUID userId, UUID activityId) {
+        log.debug("ActivityDelete 요청: userId: {}, activityId: {}", userId, activityId);
+
+        D document = getRepository().findById(userId)
+            .orElseThrow(() -> new IllegalArgumentException("Document not found for userId: " + userId));
+
+        List<T> activities = getActivityList(document);
+        int beforeSize = activities.size();
+
+        activities.removeIf(activity -> getActivityId(activity).equals(activityId));
+
+        getRepository().save(document);
+
+        log.debug("ActivityDelete 성공 userId: {}, 삭제된 개수: {}", userId, beforeSize - activities.size());
+    }
 }
