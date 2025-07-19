82db9a6a730b206ffeb11f48d7087c96c2d8ed4e
Chaewon3Lee <coduszxcv@gmail.com>
2025-07-19 15:38:57 +0900

    [MN-27] chore: flapdoodle 설정 수정

--- src/test/java/com/sprint/mission/sb03monewteam1/Sb03MonewTeam1ApplicationTests.java
+++ src/test/java/com/sprint/mission/sb03monewteam1/Sb03MonewTeam1ApplicationTests.java
@@ -5,10 +5,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.sprint.mission.sb03monewteam1.config.LoadTestEnv;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(properties = "de.flapdoodle.mongodb.embedded.version=5.0.5")
+@SpringBootTest(properties = "de.flapdoodle.mongodb.embedded.version=4.6.2")
 @EnableAsync
 @LoadTestEnv
+@ActiveProfiles("test")
 class Sb03MonewTeam1ApplicationTests {
 
     @Test
