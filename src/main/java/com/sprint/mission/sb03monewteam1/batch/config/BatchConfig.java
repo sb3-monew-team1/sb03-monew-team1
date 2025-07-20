7e5432235f90fb4db367abb5034e65e7bf3b2714
NINE <99791512+NINE-J@users.noreply.github.com>
2025-07-20 15:02:41 +0900

    [MN-48] chore: Batch Job 커넥션 풀 조정
    - 각 Batch Job이 독립적 비동기로 실행될 수 있게 설정
    - BatchConfig으로 격리 수준 명시

---
+++ src/main/java/com/sprint/mission/sb03monewteam1/batch/config/BatchConfig.java
@@ -0,0 +1,25 @@
+package com.sprint.mission.sb03monewteam1.batch.config;
+
+import javax.sql.DataSource;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+
+    @Bean
+    public JobRepository jobRepository(DataSource dataSource,
+        PlatformTransactionManager transactionManager) throws Exception {
+        JobRepositoryFactoryBean factory = new JobRepositoryFactoryBean();
+        factory.setDataSource(dataSource);
+        factory.setTransactionManager(transactionManager);
+        factory.setIsolationLevelForCreate("ISOLATION_READ_COMMITTED");
+        factory.afterPropertiesSet();
+        return factory.getObject();
+    }
+}
