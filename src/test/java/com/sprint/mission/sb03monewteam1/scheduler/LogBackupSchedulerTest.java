99ac06b9b722bc49798938c367c43ff1bf74f346
NINE <99791512+NINE-J@users.noreply.github.com>
2025-07-22 13:23:17 +0900

    [MN-46] test: LogBackupJob 테스트 케이스 추가

--- src/test/java/com/sprint/mission/sb03monewteam1/scheduler/LogBackupSchedulerTest.java
+++ src/test/java/com/sprint/mission/sb03monewteam1/scheduler/LogBackupSchedulerTest.java
@@ -1,11 +1,16 @@
 package com.sprint.mission.sb03monewteam1.scheduler;
 
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.BDDMockito.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.mock;
+import static org.mockito.BDDMockito.never;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.BDDMockito.times;
+import static org.mockito.BDDMockito.verify;
+import static org.mockito.BDDMockito.willThrow;
 
 import java.util.Collections;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -37,5 +42,25 @@ class LogBackupSchedulerTest {
 
         scheduler.runLogBackupJob();
         then(jobLauncher).should(times(1)).run(any(), any());
+    }
+
+    @Test
+    void 이미_실행중이면_jobLauncher가_호출되지_않는다() throws Exception {
+        given(jobExplorer.findRunningJobExecutions("logBackupJob"))
+            .willReturn(
+                Collections.singleton(mock(org.springframework.batch.core.JobExecution.class)));
+
+        scheduler.runLogBackupJob();
+
+        verify(jobLauncher, never()).run(any(), any());
+    }
+
+    @Test
+    void jobLauncher_실행중_예외발생해도_예외_던지지_않는다() throws Exception {
+        given(jobExplorer.findRunningJobExecutions("logBackupJob"))
+            .willReturn(Collections.emptySet());
+        willThrow(new RuntimeException("실패")).given(jobLauncher).run(any(), any());
+
+        assertDoesNotThrow(() -> scheduler.runLogBackupJob());
     }
 }
