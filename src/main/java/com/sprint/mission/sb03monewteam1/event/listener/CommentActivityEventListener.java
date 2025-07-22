71a48c626a5dc6f2361f302f4621eb55c69f4508
Chaewon3Lee <coduszxcv@gmail.com>
2025-07-22 23:15:25 +0900

    [MN-73] feat: 사용자 활동 내역 도메인 모델 업데이트 - 댓글 좋아요 시 연관 모델 업데이트 구현

--- src/main/java/com/sprint/mission/sb03monewteam1/event/listener/CommentActivityEventListener.java
+++ src/main/java/com/sprint/mission/sb03monewteam1/event/listener/CommentActivityEventListener.java
@@ -8,6 +8,7 @@ import com.sprint.mission.sb03monewteam1.dto.CommentActivityDto;
 import com.sprint.mission.sb03monewteam1.event.CommentActivityCreateEvent;
 import com.sprint.mission.sb03monewteam1.event.CommentActivityDeleteEvent;
 import com.sprint.mission.sb03monewteam1.event.CommentActivityUpdateEvent;
+import com.sprint.mission.sb03monewteam1.event.CommentLikeCountUpdateEvent;
 import com.sprint.mission.sb03monewteam1.event.UserNameUpdateEvent;
 import com.sprint.mission.sb03monewteam1.repository.mongodb.CommentActivityRepository;
 import java.util.ArrayList;
@@ -98,5 +99,21 @@ public class CommentActivityEventListener extends AbstractActivityEventListener<
 
         log.info("댓글 활동 UserNameUpdateEvent 리스너 실행 완료 userId={}, 수정된 문서 수={}", userId,
             result.getModifiedCount());
+    }
+
+    @Async
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handleCommentLikeCountChangedEvent(CommentLikeCountUpdateEvent event) {
+        UUID commentId = event.commentId();
+        long newLikeCount = event.newLikeCount();
+
+        log.debug("댓글 좋아요 수 변경 이벤트 실행: commentId={}, newLikeCount={}", commentId, newLikeCount);
+
+        Query query = new Query(Criteria.where("comments.id").is(commentId));
+        Update update = new Update().set("comments.$.likeCount", newLikeCount);
+
+        UpdateResult result = mongoTemplate.updateMulti(query, update, CommentActivity.class);
+
+        log.info("댓글 좋아요 수 변경 이벤트 완료: commentId={}, 업데이트된 문서 수={}", commentId, result.getModifiedCount());
     }
 }
--- src/main/java/com/sprint/mission/sb03monewteam1/event/listener/CommentLikeActivityEventListener.java
+++ src/main/java/com/sprint/mission/sb03monewteam1/event/listener/CommentLikeActivityEventListener.java
@@ -1,12 +1,14 @@
 package com.sprint.mission.sb03monewteam1.event.listener;
 
+import static org.springframework.transaction.event.TransactionPhase.AFTER_COMMIT;
+
 import com.mongodb.client.result.UpdateResult;
 import com.sprint.mission.sb03monewteam1.document.CommentLikeActivity;
-import com.sprint.mission.sb03monewteam1.dto.CommentActivityDto;
 import com.sprint.mission.sb03monewteam1.dto.CommentLikeActivityDto;
 import com.sprint.mission.sb03monewteam1.event.CommentActivityUpdateEvent;
 import com.sprint.mission.sb03monewteam1.event.CommentLikeActivityCreateEvent;
 import com.sprint.mission.sb03monewteam1.event.CommentLikeActivityDeleteEvent;
+import com.sprint.mission.sb03monewteam1.event.CommentLikeCountUpdateEvent;
 import com.sprint.mission.sb03monewteam1.event.UserNameUpdateEvent;
 import com.sprint.mission.sb03monewteam1.repository.mongodb.CommentLikeActivityRepository;
 import java.util.ArrayList;
@@ -20,7 +22,6 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
@@ -59,7 +60,7 @@ public class CommentLikeActivityEventListener extends AbstractActivityEventListe
     }
 
     @Async
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @TransactionalEventListener(phase = AFTER_COMMIT)
     public void handleCreateEvent(CommentLikeActivityCreateEvent event) {
         log.debug("CommentLikeActivityCreateEvent 리스너 실행: {}", event);
 
@@ -68,14 +69,14 @@ public class CommentLikeActivityEventListener extends AbstractActivityEventListe
 
 
     @Async
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @TransactionalEventListener(phase = AFTER_COMMIT)
     public void handleDeleteEvent(CommentLikeActivityDeleteEvent event) {
         log.debug("CommentLikeActivityDeleteEvent 리스너 실행: {}", event);
         deleteUserActivity(event.userId(), event.commentId());
     }
 
     @Async
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @TransactionalEventListener(phase = AFTER_COMMIT)
     public void handleUserNameUpdateForLikes(UserNameUpdateEvent event) {
         UUID userId = event.userId();
         String newUserName = event.newUserName();
@@ -100,7 +101,7 @@ public class CommentLikeActivityEventListener extends AbstractActivityEventListe
     }
 
     @Async
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @TransactionalEventListener(phase = AFTER_COMMIT)
     public void handleCommentActivityUpdateForLikes(CommentActivityUpdateEvent event) {
         UUID commentId = event.commentId();
         String updatedContent = event.commentActivityDto().content();
@@ -122,5 +123,23 @@ public class CommentLikeActivityEventListener extends AbstractActivityEventListe
             log.info("댓글 좋아요 활동 CommentActivityUpdateEvent 리스너 실행 완료 commentId={}, 수정된 문서 수={}",
                 commentId, result.getModifiedCount());
         }
+    }
+
+    @Async
+    @TransactionalEventListener(phase = AFTER_COMMIT)
+    public void handleLikeCountChanged(CommentLikeCountUpdateEvent event) {
+        UUID commentId = event.commentId();
+        long newLikeCount = event.newLikeCount();
+
+        log.debug("CommentLikeCountChangedEvent 리스너 실행: commentId={}, newLikeCount={}", commentId,
+            newLikeCount);
+
+        Query query = new Query(Criteria.where("commentLikes.commentId").is(commentId));
+        Update update = new Update().set("commentLikes.$.commentLikeCount", newLikeCount);
+
+        UpdateResult result = mongoTemplate.updateMulti(query, update, CommentLikeActivity.class);
+
+        log.info("댓글 좋아요 활동 CommentLikeCountChangedEvent 리스너 실행 완료: commentId={}, 수정 문서 수={}", commentId,
+            result.getModifiedCount());
     }
 }
