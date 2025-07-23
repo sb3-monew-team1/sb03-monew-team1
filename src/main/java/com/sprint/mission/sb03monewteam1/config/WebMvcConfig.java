36b81e5f814102e46cd5d766793d2289e4560ce8
pureod <sjo06102@gmail.com>
2025-07-23 16:57:49 +0900

    [MN-81] feat: Interceptor를 통한 Client IP 로깅

--- src/main/java/com/sprint/mission/sb03monewteam1/config/WebMvcConfig.java
+++ src/main/java/com/sprint/mission/sb03monewteam1/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.sprint.mission.sb03monewteam1.config;
 
+import com.sprint.mission.sb03monewteam1.interceptor.IpLoggingInterceptor;
 import com.sprint.mission.sb03monewteam1.interceptor.LoginInterceptor;
 import com.sprint.mission.sb03monewteam1.interceptor.MDCLoggingInterceptor;
 import lombok.NonNull;
@@ -14,15 +15,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Profile("!test")
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    private final IpLoggingInterceptor ipLoggingInterceptor;
     private final LoginInterceptor loginInterceptor;
     private final MDCLoggingInterceptor mdcLoggingInterceptor;
 
     @Override
     public void addInterceptors(@NonNull InterceptorRegistry registry) {
+        registry.addInterceptor(ipLoggingInterceptor)
+            .order(1)
+            .addPathPatterns("/api/**");
+
         registry.addInterceptor(loginInterceptor)
+            .order(2)
             .addPathPatterns("/api/**")
             .excludePathPatterns("/api/users/login", "/api/users");
+
         registry.addInterceptor(mdcLoggingInterceptor)
+            .order(3)
             .addPathPatterns("/api/**");
     }
 }
